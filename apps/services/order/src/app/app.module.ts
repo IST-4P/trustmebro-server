1f1c6175f3a26fce10711ef390da8c52554f5c0b
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2025-12-24 19:44:41 +0700

    Update

--- apps/services/order/src/app/app.module.ts
+++ apps/services/order/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { KafkaModule } from '@common/kafka/kafka.module';
 import { Module } from '@nestjs/common';
 import { ClientsModule } from '@nestjs/microservices';
 import { OrderGrpcController } from './controllers/order-grpc.controller';
+import { PrismaModule } from './prisma/prisma.module';
 import { OrderRepository } from './repositories/order.repository';
 import { OrderService } from './services/order.service';
 
 @Module({
   imports: [
+    PrismaModule,
     ClientsModule.register([
       GrpcClientProvider(GrpcService.PRODUCT_SERVICE),
       GrpcClientProvider(GrpcService.CART_SERVICE),
